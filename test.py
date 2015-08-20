69f7702d30639fe03ee06f56081ed925fc5183b9
Yousuke Utsumi <youtsumi@gmail.com>
2015-08-20 11:49:16 +0900

    third commit

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 from multiprocessing import Pool
 ccds = range(9)+range(10,104)
 
+class Ccd:
+    def __init__(self,x):
+	self.x=x
+    def process(self):
+	self.ret=self.x**2
+
 def onechip(x):
-    return x*x
+    ccd=Ccd(x)
+    ccd.process()
+    return ccd
 
 if __name__ == '__main__':
-    pool = Pool(processes=4)
+    pool = Pool(processes=16)
     result = pool.map(onechip, ccds)
-    print result
+    for ccd in  result:
+	print ccd.x, ccd.ret
