b764a261d3e19c680723ce77895f9c85ebdcb2b9
Yousuke Utsumi <youtsumi@gmail.com>
2015-08-20 12:20:17 +0900

    third commit

--- test.py
+++ test.py
@@ -1,15 +1,26 @@
 from multiprocessing import Pool
+import numpy
 ccds = range(9)+range(10,104)
 
 class Ccd:
     def __init__(self,x):
 	self.x=x
-    def process(self):
+
+    def processA(self):
+	"""write something what you want to do"""
 	self.ret=self.x**2
+
+    def processB(self,thresh):
+	"""write something what you want to do"""
+	if self.ret>thresh:
+	    self.ret=numpy.sqrt(self.x)
+	else:
+	    self.ret=0
 
 def onechip(x):
     ccd=Ccd(x)
-    ccd.process()
+    ccd.processA()
+    ccd.processB(1024)
     return ccd
 
 if __name__ == '__main__':
